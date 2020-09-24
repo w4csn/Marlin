04911d2e05a5e2baa04756668e731456bc4da96e
Scott Newton <snewton86@gmail.com>
2020-09-24 08:09:16 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -745,7 +745,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 93 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 103 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.57, 80.80, 800, 103 }
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
